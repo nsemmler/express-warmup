e7280d6ff820785ba3eb1e220078d50dadb77ca2
Nate <ndsunc@gmail.com>
2018-06-22 09:19:25 -0700

    Initial commit - created generic server with GET /ping route

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+// API
+app.get('/ping', function(req, res, next) {
+  res.status(200).send({ message: 'pong!' })
+})
+
+app.use((req, res, next) => {
+  next({ status: 404, message: 'Route not found' })
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+
+  const errToReturn = {}
+  errToReturn.status = err.status || 500
+  errToReturn.message = err.message || 'Something went wrong'
+  if (process.env.NODE_ENV === 'production') errToReturn.stack = err.stack
+
+  res.status(errToReturn.status).send(errToReturn)
+})
+
+const listener = () => `Listening on port ${port}`
+app.listen(port, listener)
+
+module.exports = app
